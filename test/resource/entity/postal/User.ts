45a24f3b1863cc4036399bc03a8c53738c339c4e
VMBindraban <vijay@sweebr.com>
2017-01-09 10:16:32 +0100

    test(resource): extend resources with properties

--- test/resource/entity/postal/User.ts
+++ test/resource/entity/postal/User.ts
@@ -1,10 +1,17 @@
 import {Mapping} from '../../../../src/Mapping';
+import {ArrayCollection} from '../../../../src/ArrayCollection';
 import {Tracker} from './Tracker';
 
 export class User {
+  public id: number;
+
+  public name: string;
+
+  public trackers: ArrayCollection<Tracker> = new ArrayCollection();
+
   public static setMapping(mapping: Mapping<User>) {
     mapping.forProperty('id').primary().increments();
     mapping.field('name', {type: 'string'});
-    mapping.forProperty('trackers').manyToMany({targetEntity: Tracker, mappedBy: 'observers'});
+    mapping.forProperty('trackers').cascade(['persist']).manyToMany({targetEntity: Tracker, mappedBy: 'observers'});
   }
 }
