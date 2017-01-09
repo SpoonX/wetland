828c3eb1cee55fd26f99a45ef55cf6daec360cec
VMBindraban <vijay@sweebr.com>
2017-01-09 10:17:00 +0100

    test(test): add tests for EntityManager, EntityRepository and Hydrator

---
+++ test/unit/EntityRepository.spec.ts
@@ -0,0 +1,171 @@
+import {Wetland} from '../../src/Wetland';
+import {EntityRepository} from '../../src/EntityRepository';
+import {Scope} from '../../src/Scope';
+import {User} from '../resource/entity/postal/User';
+import {Tracker} from '../resource/entity/postal/Tracker';
+import {assert} from 'chai';
+
+function wetland(): Wetland {
+  return new Wetland({
+    entities: [User],
+    stores  : {
+      defaultStore: {
+        client    : 'mysql',
+        connection: {
+          user    : 'root',
+          host    : '127.0.0.1',
+          database: 'wetland_test'
+        }
+      }
+    }
+  });
+}
+
+function getManager(): Scope {
+ return wetland().getManager();
+}
+
+function getRepository(): EntityRepository<User> {
+  return getManager().getRepository(User);
+}
+
+let createdUsers = [{
+  id      : 1,
+  name    : 'foo',
+  trackers: []
+}, {
+  id      : 2,
+  name    : 'bar',
+  trackers: []
+}];
+
+let createdUsersWithTrackers = [{
+  id      : 1,
+  name    : 'foo',
+  trackers: [{
+    id       : 1,
+    observers: [],
+    status   : 2
+  }]
+}, {
+  id      : 2,
+  name    : 'bar',
+  trackers: [{
+    id       : 1,
+    observers: [],
+    status   : 2
+  }]
+}];
+
+describe('EntityRepository', () => {
+  describe('.constructor()', () => {
+    it('should construct a new repository', () => {
+      let repo = getRepository();
+
+      assert.property(repo, 'entityManager');
+      assert.property(repo, 'entity');
+      assert.property(repo, 'mapping');
+    });
+  });
+
+  describe('.getQueryBuilder()', () => {
+    it('should return retrieve a queryBuilder', () => {
+      let entityAlias = getRepository().getQueryBuilder();
+      assert.propertyVal(entityAlias, 'alias', 'user');
+
+      let fooAlias = getRepository().getQueryBuilder('foo')
+      assert.propertyVal(fooAlias, 'alias', 'foo');
+    });
+  });
+
+  describe('.find()', () => {
+    it('should find all entities', () => {
+      let newUser     = new User();
+      let newObserver = new User();
+      let newTracker  = new Tracker();
+
+      newObserver.name = 'bar';
+
+      newTracker.status = 2;
+      newTracker.observers.add(newObserver);
+
+      newUser.name = 'foo';
+      newUser.trackers.add(newTracker);
+
+      return getManager().persist(newUser).flush()
+        .then(() => getRepository().find())
+        .then(users => assert.deepEqual(users, createdUsers));
+    });
+
+    it('should not find entities based on criteria', () => {
+      return getRepository().find({name: 'foobar'})
+        .then(users => assert.isNull(users));
+    });
+
+    it('should find entities based on criteria', () => {
+      return getRepository().find({name: 'foo'})
+        .then(users => assert.deepEqual(users, [createdUsers[0]]));
+    });
+
+    it('should find entities with alias option', () => {
+      return getRepository().find(null, {alias: 'foo'})
+        .then(users => assert.deepEqual(users, createdUsers));
+    });
+
+    it('should find entities with populateAll option', () => {
+      return getRepository().find(null, {populate: true})
+        .then(users => assert.deepEqual(users, createdUsersWithTrackers));
+    });
+
+    it('should find entities with specific populate option', () => {
+      return getRepository().find(null, {populate: 'trackers'})
+        .then(users => assert.deepEqual(users, createdUsersWithTrackers));
+    });
+
+    it('should find entities with deep populate option', () => {
+      return getRepository().find(null, {populate: ['trackers', 'trackers.observers']})
+        .then(users => {
+          assert.propertyVal(users[0].trackers[0].observers[0], 'id', 1);
+          assert.propertyVal(users[0].trackers[0].observers[0], 'name', 'foo');
+          assert.property(users[0].trackers[0].observers[0], 'trackers');
+        });
+    });
+  });
+
+  describe('.findOne()', () => {
+    it('should find one entity', () => {
+      return getRepository().findOne()
+        .then(user => assert.deepEqual(user, createdUsers[0]));
+    });
+
+    it('should find a entity with alias option', () => {
+      return getRepository().findOne(null, {alias: 'foo'})
+        .then(user => assert.deepEqual(user, createdUsers[0]));
+    });
+
+    it('should find a entity with on primary key as criteria', () => {
+      return getRepository().findOne(2)
+        .then(user => assert.deepEqual(user, createdUsers[1]));
+    });
+
+    it('should not find a entity with on primary key as criteria', () => {
+      return getRepository().findOne(99)
+        .then(user => assert.isNull(user));
+    });
+
+    it('should find a entity with string as criteria', () => {
+      return getRepository().findOne('1')
+        .then(user => assert.deepEqual(user, createdUsers[0]));
+    });
+  });
+
+  describe('.applyOptions()', () => {
+    it('should apply options', () => {
+      let appliedOptions = getRepository().applyOptions(getRepository().getQueryBuilder(), {
+        select: 'name'
+      });
+
+      assert.notDeepEqual(appliedOptions, getRepository().getQueryBuilder());
+    });
+  });
+});
---
+++ test/unit/Hydrator.spec.ts
@@ -0,0 +1,68 @@
+import {Wetland} from '../../src/Wetland';
+import {Scope} from '../../src/Scope';
+import {Hydrator} from '../../src/Hydrator';
+import {User} from '../resource/entity/postal/User';
+import {Tracker} from '../resource/entity/postal/Tracker';
+import {assert} from 'chai';
+
+function getHydrator(): Hydrator {
+  return new Hydrator(getManager());
+}
+
+function getManager(): Scope {
+  let wetland = new Wetland();
+
+  return wetland.getManager();
+}
+
+describe('Hydrator', () => {
+  describe('.consturctor()', () => {
+    it('should define constructor properties', () => {
+      let hydrator = getHydrator();
+
+      assert.property(hydrator, 'unitOfWork');
+      assert.property(hydrator, 'entityManager');
+      assert.property(hydrator, 'identityMap');
+    });
+  });
+
+  describe('.fromSchema()', () => {
+    it('should map to entities', () => {
+      let entity = getHydrator().fromSchema({
+        name: 'foo'
+      }, User);
+
+      assert.propertyVal(entity, 'name', 'foo');
+    });
+
+    it('should not map invalid value to entities', () => {
+      let entity = getHydrator().fromSchema({
+        bar: 'foo'
+      }, User);
+
+      assert.notProperty(entity, 'bar');
+    });
+
+    it('should map to entities with empty object', () => {
+      let entity = getHydrator().fromSchema({
+        name: 'foo'
+      }, {});
+
+      assert.notProperty(entity, 'name');
+    });
+  });
+
+  describe('.addRecipe', () => {
+    it('should add a recipe', () => {
+      let recipe = getHydrator().addRecipe(null, 'foo', getManager().getMapping(User), 'single');
+
+      assert.deepEqual(recipe.primaryKey, {alias: 'foo.id', property: 'id'});
+      assert.isNull(recipe.parent);
+      assert.isFalse(recipe.hydrate);
+      assert.equal(recipe.type, 'single');
+      assert.isObject(recipe.columns);
+      assert.isUndefined(recipe.property);
+      assert.equal(recipe.alias, 'foo');
+    });
+  });
+});
